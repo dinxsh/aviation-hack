c6bea408e5b5dbe0f138ea48735a5df2f341ec0e
dinesh <talwadkerdinesh@gmail.com>
2023-09-03 18:42:36 +0530

    feat: moved nav to components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import Navbar from '../components/navbar'
+import Navbar from '@/components/navbar/'
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
